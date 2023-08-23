2fea178b1c9da8d0ca21d1ecff7a75b30b9900e5
Björn Petersen <git@bjoernpetersen.net>
2023-08-23 21:59:49 +0200

    Add frontend-nevermind

---
+++ repo_frontend-nevermind.tf
@@ -0,0 +1,8 @@
+module "frontend_nevermind_repo" {
+  source              = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name                = "frontend-nevermind"
+  default_branch_name = "master"
+  blocked_branches    = []
+  is_archived         = true
+  enable_actions      = false
+}
