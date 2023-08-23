3f16f6814f4548087e0652a48fa45d59ddb74d3a
Björn Petersen <git@bjoernpetersen.net>
2023-08-23 21:36:47 +0200

    Change API description

--- repo_api.tf
+++ repo_api.tf
@@ -1,7 +1,7 @@
 module "api_repo" {
   source      = "github.com/BlindfoldedSurgery/terraform-repo-module"
   name        = "api"
-  description = "The official tim-hat-die-hand-an-der-maus API"
+  description = "The official API"
   required_status_checks = [
     "Test and Lint",
     "Container Build",
