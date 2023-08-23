e8b33320ad2ad9e483fa9dba4baf623bc806088c
Björn Petersen <git@bjoernpetersen.net>
2023-08-23 21:38:16 +0200

    Fix configs

--- repo_plex-library-update-notifier.tf
+++ repo_plex-library-update-notifier.tf
@@ -1,8 +1,6 @@
 module "plex_library_update_notifier_repo" {
-  source              = "github.com/BlindfoldedSurgery/terraform-repo-module"
-  name                = "plex-library-update-notifier"
-  default_branch_name = "master"
-  blocked_branches    = []
+  source = "github.com/BlindfoldedSurgery/terraform-repo-module"
+  name   = "plex-library-update-notifier"
   required_status_checks = [
     "lint",
     "build_push_docker",
--- repo_streamingprovider-resolver.tf
+++ repo_streamingprovider-resolver.tf
@@ -2,6 +2,7 @@ module "streamingprovider_resolver_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module"
   name                = "streamingprovider-resolver"
   default_branch_name = "master"
+  blocked_branches    = []
   required_status_checks = [
     "helm lint",
   ]
