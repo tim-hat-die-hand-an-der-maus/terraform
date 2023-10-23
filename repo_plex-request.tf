fde03f9b2d76460ac7b7a3832821a8bbd3f79d22
Torben Carstens <git@carstens.tech>
2023-10-23 15:11:13 +0200

    Change plex-request default branch to main

--- repo_plex-request.tf
+++ repo_plex-request.tf
@@ -1,7 +1,7 @@
 module "plex_request_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v3.0.0"
   name                = "plex-request"
-  default_branch_name = "master"
+  default_branch_name = "main"
   blocked_branches    = []
   required_status_checks = [
     "lint",
