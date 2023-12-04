87b7206b586af6f8c194ac097005eaecb177adf3
Torben Carstens <git@carstens.tech>
2023-12-04 23:45:52 +0100

    Move telegram-bot default branch to main

--- repo_telegram-bot.tf
+++ repo_telegram-bot.tf
@@ -1,7 +1,7 @@
 module "telegram_bot_repo" {
   source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
   name                = "telegram-bot"
-  default_branch_name = "master"
+  default_branch_name = "main"
   blocked_branches    = []
   required_status_checks = [
     "lint",
