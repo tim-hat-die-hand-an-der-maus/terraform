41d40642211ba5eefd66ccb9104fb75651b1f06b
Björn Petersen <git@bjoernpetersen.net>
2023-12-17 14:52:54 +0100

    python-sdk: Add required status checks

--- repo_python-sdk.tf
+++ repo_python-sdk.tf
@@ -1,4 +1,11 @@
 module "python_sdk_repo" {
-  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
-  name   = "python-sdk"
+  source                                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v5.0.0"
+  name                                   = "python-sdk"
+  allow_default_branch_protection_bypass = false
+  required_status_checks = [
+    "check-commits / check",
+    "test (3.11)",
+    "lint (3.11) / lint",
+    "validate-renovate-config / validate",
+  ]
 }
