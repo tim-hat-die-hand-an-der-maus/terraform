45347190673ad9641f9ca73e5ec5a30b85c42fd3
Björn Petersen <git@bjoernpetersen.net>
2024-04-06 13:01:43 +0200

    Remove station-bot repo

--- repo_station-bot.tf
+++
@@ -1,8 +0,0 @@
-module "station_bot_repo" {
-  source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v6.0.0"
-  name   = "station-bot"
-  required_status_checks = [
-    "lint",
-    "build",
-  ]
-}
