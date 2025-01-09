9ff0c5e70684f5a1e05d631ec8f4fc2b2f717d2d
Björn Petersen <git@bjoernpetersen.net>
2025-01-09 22:53:27 +0100

    Add mirror repo

---
+++ repo_actions_container.tf
@@ -0,0 +1,10 @@
+module "actions_container_repo" {
+  source      = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.0.2"
+  name        = "actions-container"
+  description = "Mirrors the tags of the BlindfoldedSurgery repo with the same name"
+  is_public   = true
+
+  enable_argocd_rules          = false
+  include_required_meta_checks = false
+  required_status_checks       = []
+}
