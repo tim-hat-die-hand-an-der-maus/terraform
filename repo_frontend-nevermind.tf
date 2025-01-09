6b1dbb4e5056be76dfafd437453c76c3c4533db1
Björn Petersen <git@bjoernpetersen.net>
2025-01-09 22:05:39 +0100

    Revert frontend-nevermind

--- repo_frontend-nevermind.tf
+++ repo_frontend-nevermind.tf
@@ -1,10 +1,18 @@
 module "frontend_nevermind_repo" {
-  source                 = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.0.2"
-  name                   = "frontend-nevermind"
-  default_branch_name    = "master"
-  blocked_branches       = []
-  required_status_checks = []
+  source              = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.0.2"
+  name                = "frontend-nevermind"
+  default_branch_name = "master"
+
+  blocked_branches = []
+  required_status_checks = [
+    "build-container-image / build",
+    "lint / lint",
+    "test / test",
+    "validate-renovate-config / validate",
+  ]
+
   protect_default_branch = false
   enable_actions         = false
   enable_argocd_rules    = false
+  is_archived            = true
 }
