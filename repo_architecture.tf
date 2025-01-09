583236b04d790db8a568b676988d14b7fa585c03
Björn Petersen <git@bjoernpetersen.net>
2025-01-09 22:10:56 +0100

    Archive some repos

--- repo_architecture.tf
+++ repo_architecture.tf
@@ -5,6 +5,7 @@ module "architecture_repo" {
   enable_argocd_rules          = false
   include_required_meta_checks = false
   protect_default_branch       = false
+  is_archived                  = true
 
   required_status_checks = []
   blocked_branches       = []
