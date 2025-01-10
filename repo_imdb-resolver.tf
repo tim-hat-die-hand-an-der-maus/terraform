b85647e34a347e3ff521c55392c5d4cd2e293bae
Björn Petersen <git@bjoernpetersen.net>
2025-01-10 20:09:26 +0100

    Warm up imdb-resolver

--- repo_imdb-resolver.tf
+++ repo_imdb-resolver.tf
@@ -1,9 +1,4 @@
 module "imdb_resolver_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.0.2"
   name   = "imdb-resolver"
-
-  blocked_branches       = []
-  required_status_checks = []
-  protect_default_branch = false
-  enable_actions         = false
 }
