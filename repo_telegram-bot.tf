59057ad4c5f424d4015518eee98b5a8897bce43b
Björn Petersen <git@bjoernpetersen.net>
2025-01-11 13:02:50 +0100

    Normal bot operations

--- repo_telegram-bot.tf
+++ repo_telegram-bot.tf
@@ -1,8 +1,4 @@
 module "telegram_bot_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v9.0.2"
   name   = "telegram-bot"
-
-  blocked_branches       = []
-  required_status_checks = []
-  protect_default_branch = false
 }
