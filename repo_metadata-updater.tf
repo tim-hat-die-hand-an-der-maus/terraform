b554464f8fe9d504793116c6982a9ee05ea229be
Björn Petersen <git@bjoernpetersen.net>
2025-10-11 18:28:07 +0200

    chore: add new archival flag to repos

--- repo_metadata-updater.tf
+++ repo_metadata-updater.tf
@@ -3,12 +3,6 @@ module "metadata_updater_repo" {
   name                = "metadata-updater"
   default_branch_name = "master"
 
-  blocked_branches       = []
-  required_status_checks = []
-
-  protect_default_branch       = false
-  enable_argocd_rules          = false
-  include_required_meta_checks = false
-  enable_actions               = false
-  is_archived                  = true
+  is_archive_prepared = true
+  is_archived         = true
 }
