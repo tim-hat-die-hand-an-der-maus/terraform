2cbc0f174a5c7e74f870e2b2ae3ec2a742f74113
Björn Petersen <git@bjoernpetersen.net>
2025-11-07 22:09:51 +0100

    Archive imdb-resolver

--- repo_imdb-resolver.tf
+++ repo_imdb-resolver.tf
@@ -2,7 +2,7 @@ module "imdb_resolver_repo" {
   source = "github.com/BlindfoldedSurgery/terraform-repo-module?ref=v10.1.1"
   name   = "imdb-resolver"
 
-  is_archived         = false
+  is_archived         = true
   is_archive_prepared = true
   is_public           = false
 }
